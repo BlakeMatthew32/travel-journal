69644de59687d82207130de80624a75ced2fa58b
Blake Matthew <79722560+BlakeMatthew32@users.noreply.github.com>
2023-06-11 15:15:51 +0100

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Header() {
+    return (
+        <header className="header">
+            <h3 className="header--title">My Travel Journal</h3>
+        </header>
+    )
+}
